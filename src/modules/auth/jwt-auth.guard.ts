a9cf2ef104c300c8631dbb5c3f324b24e3d52b2e
Jyothika Suresh <62376533+JyothikaS@users.noreply.github.com>
2024-02-02 19:40:35 +0530

    Update jwt-auth.guard.ts

--- src/modules/auth/jwt-auth.guard.ts
+++ src/modules/auth/jwt-auth.guard.ts
@@ -3,4 +3,4 @@ import { Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt')  {} //local is the startegy name
+export class JwtAuthGuard extends AuthGuard('jwt')  {} //jwt is the startegy name
